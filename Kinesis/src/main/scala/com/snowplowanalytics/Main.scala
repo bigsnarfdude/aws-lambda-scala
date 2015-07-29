c3f4e97ca706e084bd1911e1337e62d0724873a2
bigsnarfdude <ohprecio@gmail.com>
2015-07-29 13:18:56 -0700

    DataSchema kinda parser in xml jackson

--- Kinesis/src/main/scala/com/snowplowanalytics/Main.scala
+++ Kinesis/src/main/scala/com/snowplowanalytics/Main.scala
@@ -5,11 +5,30 @@ import com.amazonaws.services.lambda.runtime.events.KinesisEvent
 import com.amazonaws.services.lambda.runtime.events.KinesisEvent.KinesisEventRecord
 import scala.collection.JavaConversions._
 
+case class DataSchema( 
+	dataSchema: Any,
+    dataSource: Any,
+    parser: Any, 
+    metricsSpec: Any, 
+    granularitySpec: Any
+)
 
 class ProcessKinesisEvents {
+    
+    import com.fasterxml.jackson.databind.ObjectMapper
+    import com.fasterxml.jackson.module.scala.DefaultScalaModule
+
+	val scalaMapper = {  
+      new ObjectMapper().registerModule(new DefaultScalaModule)
+    }
+
+
     def recordHandler(event: KinesisEvent) {
     for (rec <- event.getRecords) {
-      println(new String(rec.getKinesis.getData.array()))
+      val record = new String(rec.getKinesis.getData.array())
+      //println(record)
+      val dataSchema = scalaMapper.readValue(record, classOf[DataSchema])
+      println(dataSchema)
     }
   }
 }
