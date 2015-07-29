9637cae0f40113b3027c152b8a7ea7d1f27dba81
bigsnarfdude <ohprecio@gmail.com>
2015-07-29 16:45:24 -0700

    working version of crappy parser

--- Kinesis/src/main/scala/com/snowplowanalytics/Main.scala
+++ Kinesis/src/main/scala/com/snowplowanalytics/Main.scala
@@ -5,13 +5,13 @@ import com.amazonaws.services.lambda.runtime.events.KinesisEvent
 import com.amazonaws.services.lambda.runtime.events.KinesisEvent.KinesisEventRecord
 import scala.collection.JavaConversions._
 
-case class DataSchema( 
-	dataSchema: Any,
-    dataSource: Any,
-    parser: Any, 
-    metricsSpec: Any, 
-    granularitySpec: Any
-)
+//case class DataSchema(dataSchema: Map[String, Any])
+case class DataSource(dataSource: Map[String,Any])
+case class Parser(parser: Map[String, Any])
+case class MetricsSpec(metricsSpec: Any) 
+case class GranularitySpec(granularitySpec: Any)
+
+case class DataSchema(dataSchema:Map[String, Any])
 
 class ProcessKinesisEvents {
     
@@ -22,13 +22,12 @@ class ProcessKinesisEvents {
       new ObjectMapper().registerModule(new DefaultScalaModule)
     }
 
-
     def recordHandler(event: KinesisEvent) {
     for (rec <- event.getRecords) {
       val record = new String(rec.getKinesis.getData.array())
-      //println(record)
       val dataSchema = scalaMapper.readValue(record, classOf[DataSchema])
       println(dataSchema)
     }
   }
 }
+
